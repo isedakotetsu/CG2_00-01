3d225772e0ebd660be5c82980c316abf46019f75
isedakotetsu <k024g0050@g.neec.ac.jp>
2025-04-16 15:46:32 +0900

    ログを出す

--- main.cpp
+++ main.cpp
@@ -1,5 +1,41 @@
 #include <windows.h>
 #include<cstdint>
+#include <string>
+#include <format>
+
+
+void Log(const std::string& message)
+{
+	OutputDebugStringA(message.c_str());
+}
+
+std::wstring ConvertString(const std::string& str) {
+	if (str.empty()) {
+		return std::wstring();
+	}
+
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0) {
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+
+std::string ConvertString(const std::wstring& str) {
+	if (str.empty()) {
+		return std::string();
+	}
+
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0) {
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
+}
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
 	WPARAM wparam, LPARAM lparam)
@@ -32,12 +68,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 	RegisterClass(&wc);
 
-	OutputDebugStringA("Hello,DirectX!\n");
-
+	Log("Hello,DirectX!\n");
 	
 
 	const int32_t kClientWidth = 1280;
 	const int32_t kClientHeight = 720;
+	Log(ConvertString(std::format(L"WSTRING{}\n", kClientWidth)));
 
 	RECT wrc = { 0, 0, kClientWidth, kClientHeight };
 
@@ -73,6 +109,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 			//ゲーム処理
 		}
 	}
+	
+
+	
+	
 
 	return 0;
 }
